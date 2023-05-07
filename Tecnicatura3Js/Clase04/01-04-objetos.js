1c0c8cb2b61e71f50c21307e43cfde16c59e56a3
piaolivera <107521407+piaolivera@users.noreply.github.com>
2023-05-07 19:05:11 -0300

    JS (video 1.2 y 1.2)

---
+++ Tecnicatura3Js/Clase04/01-04-objetos.js
@@ -0,0 +1,14 @@
+let x = 10; //variable de tipo primitiva
+console.log(x.length);
+
+//objeto
+let persona = {
+    nombre: 'Carlos',
+    apellido: 'Gil',
+    email: 'cgil@gmail.com',
+    edad: 30,
+}
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.email);
+console.log(persona.edad);
