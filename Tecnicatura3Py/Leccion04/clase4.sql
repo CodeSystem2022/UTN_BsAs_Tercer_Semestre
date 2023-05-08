f28a8b1eccd8ea37076beeabc4ee41e02afabeef
Alicia Viviana Montenegro <105835509+vmgith@users.noreply.github.com>
2023-05-08 00:44:18 -0300

    Add files via upload
    Consultas en  Postgresql

---
+++ Tecnicatura3Py/Leccion04/clase4.sql
@@ -0,0 +1,14 @@
+--Consulta informacion seleccionada
+--SELECT * FROM personas WHERE id_persona=2
+--SELECT * FROM personas WHERE id_persona in (1,2)
+--Agregar un nuevo registro
+--insert into personas(nombre,apellido,email) values ('Susana','Lara','slara@mail.com')
+--Modificar datos de un registro
+--update persona set nombre ='Maria',email='maria@mial.com' where id_persona=3
+--eliminar registros
+DELETE from personas where id_persona=3
+
+
+
+
+
