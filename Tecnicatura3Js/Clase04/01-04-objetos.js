141e5e6e2a75354b23e5231aba5e15bae32f4363
Darío Vega <sergiodariovega@gmail.com>
2023-05-07 23:40:50 -0300

    Add files via upload
    Clase 4 videos 1.3 y 1.4

--- Tecnicatura3Js/Clase04/01-04-objetos.js
+++ Tecnicatura3Js/Clase04/01-04-objetos.js
@@ -1,14 +1,27 @@
-let x = 10; //variable de tipo primitiva
+let x = 10;
 console.log(x.length);
 
-//objeto
+//Objeto
 let persona = {
     nombre: 'Carlos',
     apellido: 'Gil',
     email: 'cgil@gmail.com',
     edad: 30,
+    nombreCompleto: function(){
+        return this.nombre+' '+this.apellido;
+    }
 }
+
 console.log(persona.nombre);
 console.log(persona.apellido);
 console.log(persona.email);
 console.log(persona.edad);
+console.log(persona);
+console.log(persona.nombreCompleto());
+
+let persona2 = new Object(); //Debe crear un nuevo onjeto en memoria
+persona2.nombre = 'Juan';
+persona2.direccion = 'Salada 14';
+persona2.telefono =  '5451254225';
+console.log(persona2.telefono)
+
