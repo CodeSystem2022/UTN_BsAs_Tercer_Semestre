58e5ebc99ec709745b04f87a900603d56c85baac
piaolivera <piaoliveraa@gmail.com>
2023-05-10 20:12:05 -0300

    Leccion04 Java InstanceOf

---
+++ Tecnicatura3Java/Leccion04/InstanceOf/src/domain/Gerente.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author Pia Olivera
+ */
+public class Gerente extends empleado{
+    private String departamento;
+    
+    public Gerente (String Nombre, double Sueldo, String departamento){
+        super(Nombre, Sueldo);
+        this.departamento = departamento;
+    }
+    
+    // SOBRE ESCRIBIMOS EL METODO 
+    @Override
+    public String obtenerDetalles(){
+        return super.obtenerDetalles()+ ", Departamento: "+ this.departamento;
+        
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+    
+}
