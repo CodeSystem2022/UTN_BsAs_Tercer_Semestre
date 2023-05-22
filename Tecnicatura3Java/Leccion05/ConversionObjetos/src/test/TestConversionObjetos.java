9adc3d7e8a20e8f5cc6f1f8c1b78ea185c54fbe4
Rasta07 <fernando.barrionuevo@outlook.com>
2023-05-22 01:24:17 -0300

    Leccion05-1
    Parte1

--- Tecnicatura3Java/Leccion05/ConversionObjetos/src/test/TestConversionObjetos.java
+++ Tecnicatura3Java/Leccion05/ConversionObjetos/src/test/TestConversionObjetos.java
@@ -1,27 +1,23 @@
+
 package test;
 
-import domain.Empleado;
-import domain.Escritor;
-import domain.TipoEscritura;
+import domain.*;
 
 public class TestConversionObjetos {
-    
-    public static void main(String[] args) {
-        Empleado empleado;
-        empleado = new Escritor("Juan", 5000, TipoEscritura.CLASICO);
-        
-        //System.out.println("empleado = " + empleado);
-        //System.out.println(empleado.obtenerDetalles());//Si queremos acceder a métodos Escritor
-        //empleado.getTipoEscritura(); No se puede hacer
-        
-        //Downcasting
-        //((Escritor) empleado).getTipoEscritura(); //Tenemos dos opciones: esta es una
-        Escritor escritor = (Escritor) empleado; //Esta es la segunda opción
-        escritor.getTipoEscritura();
-        
-        //Upcasting
-        Empleado empleado2 = escritor;
-        System.out.println(empleado2.obtenerDetalles());
-    }
-    
+       public static void main(String[]args){
+           Empleado empleado;
+           empleado= new Escritor ("Juan",5000,TipoEscritura.CLASICO);
+           //System.out.println("empleado="+empleado);
+           System.out.println(empleado.obtenerDetalles());//Si queremos acceder aa metodos escritor
+           //empleado.getTipoEscritura(); no se puede hacer
+           //Downcasting
+           //((Escritor)empleado).getTipoEscritura();//Tenemos 2 opciones: esta es una
+           Escritor escritor= (Escritor)empleado;//esta es la segunda opcion
+           escritor.getTipoEscritura();
+           
+           //Upcasting
+          Empleado empleado2 = escritor;
+           System.out.println(empleado2.obtenerDetalles());
+
+        }
 }
