430c85278934cbe1e207694279d76d1221af454a
Viviana A. Montenegro <vivy.montenegro@gmail.com>
2023-06-10 00:44:05 -0300

    Clase 8 Capa de Datos: Logging y Postgresql Parte 2
    8.3 Creación de la Clase Conexion: Parte 1, 2 y 3

---
+++ Tecnicatura3Py/Leccion07/capa_datos_persona/conexion.py
@@ -0,0 +1,52 @@
+
+#8.3 Creación de la Clase Conexion
+import logging
+import sys
+import logging
+import psycopg2 as bd
+class Conexion:
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _conexion = None
+    _cursor = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host=cls._HOST,
+                                            user=cls._USERNAME,
+                                            password=cls._PASSWORD,
+                                            port=cls._DB_PORT,
+                                            database=cls._DATABASE)
+                logging.debug(f'Conexion Existosa: {cls._conexion}')
+                return cls._conexion
+            except Exception as e:
+                log.error(f'Ocurrio un error: {e}')
+                sys.exit()
+        else:
+            return cls._conexion
+
+
+
+    @classmethod
+    def obtenerCursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtenerConexion().cursor()
+                log.debug(f'se abrio correctamente el cursor: {cls._cursor}')
+                return cls._cursor
+            except Exception as e:
+                log.error(f'ocurrio un error:  {e}')
+                sys.exit()
+        else:
+            return cls._cursor
+
+#prueba de errores
+if __name__ == '__main__':
+    Conexion.obtenerConexion()
+    Conexion.obtenerCursor()
+
