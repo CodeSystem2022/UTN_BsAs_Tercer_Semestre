aa2d1f7c1261f533bfc6d2e707e9caf23b088ee2
Darío Vega <sergiodariovega@gmail.com>
2023-06-10 20:24:11 -0300

    Java Clase 9 Proyecto Calculadora

---
+++ Tecnicatura3Java/Leccion09/Calculadora/src/CalculadoraUTN.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class CalculadoraUTN {
+    public static void main(String[] args) {
+
+        while(true){
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println();
+            System.out.println("********** Aplicación Calculadora ***********");
+            System.out.println();
+
+           mostrarMenu();
+
+            try {
+
+                var operacion = Integer.parseInt(sc.nextLine());
+
+                if(operacion >= 1 && operacion <= 4){
+
+                    ejecutarOperación(operacion,sc);
+
+                } else if (operacion == 5){
+
+                    System.out.println("Hasta pronto...");
+                    break; // Rompe el ciclo y sale
+
+                } else {
+
+                    System.out.println("Opción errónea: " + operacion);
+
+                } // FIn if
+
+                // Imprimimos un salto de linea antes de salir del ciclo
+                System.out.println();
+
+            }catch (Exception e){
+                System.out.println("Ocurrió un error: " + e.getMessage());
+            }
+
+        } //Fin while
+
+    } //Fin main
+
+    private static void mostrarMenu(){
+
+        //Mostramos el menú
+        System.out.println(
+                "1. Suma\n" +
+                        "2. Resta\n" +
+                        "3. Multiplicación\n" +
+                        "4. División\n" +
+                        "5. Salir\n"
+        );
+
+        System.out.print("Operación a realizar?: ");
+    } // FIn mostrarMenu
+
+    private static void ejecutarOperación(int operacion, Scanner sc){
+
+        System.out.print("Digite el valor para el operando1: ");
+        var operando1 = Double.parseDouble(sc.nextLine());
+
+        System.out.print("Digite el valor para el operando2: ");
+        var operando2 = Double.parseDouble(sc.nextLine());
+
+        double resultado;
+
+        switch (operacion){
+            case 1:
+                resultado = operando1 + operando2;
+                System.out.println("El resultado de la suma entre " + operando1 + " y " + operando2 + " es: " + resultado);
+                break;
+            case 2:
+                resultado = operando1 - operando2;
+                System.out.println("El resultado de la resta entre " + operando1 + " y " + operando2 + " es: " + resultado);
+                break;
+            case 3:
+                resultado = operando1 * operando2;
+                System.out.println("El resultado de la multiplicación entre " + operando1 + " y " + operando2 + " es: " + resultado);
+                break;
+            case 4:
+                try {
+                    resultado = operando1 / operando2;
+                    System.out.println("El resultado de la división entre " + operando1 + " y " + operando2 + " es: " + resultado);
+                }catch (ArithmeticException e){
+                    System.out.println("No se puede dividir por cero");
+                }
+                break;
+            default:
+                System.out.println("Opción errónea: " + operacion);
+        } //Fin switch
+    } // Fin ejecutarOperacion
+
+} //Fin class
